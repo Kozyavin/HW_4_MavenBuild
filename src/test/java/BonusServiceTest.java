3b1493d3eed756a4534eb164ffa59ad53562f469
Kozyavin V <kioci@yandex.ru>
2023-04-13 19:57:48 +0300

    AddTestCase

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -16,6 +16,19 @@ public class BonusServiceTest {
     }
 
     @Test
+    void shouldCalculateForRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+        // подготавливаем данные:
+        long amount = 1_000_000;
+        boolean registered = true;
+        long expected = 500;
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
     void shouldCalculateForUnregisteredAndUnderLimit() {
         BonusService service = new BonusService();
         // подготавливаем данные:
@@ -29,23 +42,10 @@ public class BonusServiceTest {
     }
 
     @Test
-    void shouldCalculateForRegisteredAndZeroAmount() {
-        BonusService service = new BonusService();
-        // подготавливаем данные:
-        long amount = 0;
-        boolean registered = true;
-        long expected = 0;
-        // вызываем целевой метод:
-        long actual = service.calculate(amount, registered);
-        // производим проверку (сравниваем ожидаемый и фактический):
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
     void shouldCalculateForUnregisteredAndOverLimit() {
         BonusService service = new BonusService();
         // подготавливаем данные:
-        long amount = 50_000;
+        long amount = 60_000;
         boolean registered = false;
         long expected = 500;
         // вызываем целевой метод:
